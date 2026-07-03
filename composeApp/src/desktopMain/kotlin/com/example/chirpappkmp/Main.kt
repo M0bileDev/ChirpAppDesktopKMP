0773ea97438e09461d020cb21000702b406946a1
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-03 23:55:22 +0200

    feature(chirpappkmp): provide App composable function

--- composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
@@ -1,5 +1,15 @@
 package com.example.chirpappkmp
 
-fun main(){
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
+fun main(){
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "ChirpDesktop"
+        ){
+            App()
+        }
+    }
 }
