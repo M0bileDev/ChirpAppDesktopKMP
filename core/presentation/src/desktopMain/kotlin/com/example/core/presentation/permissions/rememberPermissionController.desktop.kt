5d3e3157c333982a22cc0825ea50e9eeb4ab32d9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-04 19:23:19 +0200

    refactor(permissions): wrap PermissionController with remember block

--- core/presentation/src/desktopMain/kotlin/com/example/core/presentation/permissions/rememberPermissionController.desktop.kt
+++ core/presentation/src/desktopMain/kotlin/com/example/core/presentation/permissions/rememberPermissionController.desktop.kt
@@ -1,8 +1,9 @@
 package com.example.core.presentation.permissions
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 
 @Composable
 actual fun rememberPermissionController(): PermissionController {
-    return PermissionController()
+    return remember { PermissionController() }
 }
