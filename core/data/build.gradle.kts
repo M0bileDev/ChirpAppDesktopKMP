a747d5644fc46dee3468b0b73230f3f21fb07630
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-05 23:24:23 +0200

    feature(gradle): create dependency of libs.ktor.client.okhttp for desktop

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -24,6 +24,12 @@ kotlin {
             }
         }
 
+        desktopMain{
+            dependencies{
+                implementation(libs.ktor.client.okhttp)
+            }
+        }
+
         androidMain {
             dependencies {
                 implementation(libs.ktor.client.okhttp)
