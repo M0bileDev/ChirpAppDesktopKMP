6e4fb9c91903a383957a5ac40ed5633318dfd62b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-05 22:58:00 +0200

    refactor(util): extract application directory to separate property with custom getter

--- core/data/src/desktopMain/kotlin/com/example/core/data/auth/createDataStore.dekstop.kt
+++ core/data/src/desktopMain/kotlin/com/example/core/data/auth/createDataStore.dekstop.kt
@@ -2,8 +2,7 @@ package com.example.core.data.auth
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import com.example.core.data.util.DesktopOS
-import com.example.core.data.util.currentOS
+import com.example.core.data.util.applicationDirectory
 import java.io.File
 
 const val APP_FOLDER_NAME = "Chirp"
@@ -13,17 +12,6 @@ const val MAC_APPLICATIONS_DIRECTORY = "Library/Application Support/"
 const val LINUX_APPLICATIONS_DIRECTORY = ".local/share/"
 
 fun createDataStore(): DataStore<Preferences> = createDataStore {
-    val userHomeDirectory = System.getProperty(HOME_DIRECTORY)
-    val applicationDirectory = when (currentOS) {
-        DesktopOS.WINDOWS -> File(System.getenv(APP_DATA), APP_FOLDER_NAME)
-        DesktopOS.MACOS -> File(
-            userHomeDirectory,
-            buildString { MAC_APPLICATIONS_DIRECTORY + APP_FOLDER_NAME })
-
-        DesktopOS.LINUX -> File(
-            userHomeDirectory,
-            buildString { LINUX_APPLICATIONS_DIRECTORY + APP_FOLDER_NAME })
-    }
 
     if (!applicationDirectory.exists()) {
         applicationDirectory.mkdirs()
