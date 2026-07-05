8ea210bc21ef08ebb89290ff296a06bcfde75d27
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-05 22:52:17 +0200

    refactor(di): register di data store factory for desktop

--- core/data/src/desktopMain/kotlin/com/example/core/data/di/CoreDataModule.desktop.kt
+++ core/data/src/desktopMain/kotlin/com/example/core/data/di/CoreDataModule.desktop.kt
@@ -1,5 +1,10 @@
 package com.example.core.data.di
 
+import com.example.core.data.auth.createDataStore
 import org.koin.dsl.module
 
-actual val platformCoreDataModule = module { }
+actual val platformCoreDataModule = module {
+    single {
+        createDataStore()
+    }
+}
