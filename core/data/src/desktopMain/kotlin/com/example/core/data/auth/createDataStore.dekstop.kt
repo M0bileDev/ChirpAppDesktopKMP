f8bd3cfb741bf4dcd1f7e9e641700892b5d8894c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-05 22:48:50 +0200

    feature(auth): provide implementation of DataStore factory for desktop

---
+++ core/data/src/desktopMain/kotlin/com/example/core/data/auth/createDataStore.dekstop.kt
@@ -0,0 +1,40 @@
+package com.example.core.data.auth
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.example.core.data.auth.DesktopDataStore.APP_DATA
+import com.example.core.data.auth.DesktopDataStore.APP_FOLDER_NAME
+import com.example.core.data.auth.DesktopDataStore.HOME_DIRECTORY
+import com.example.core.data.auth.DesktopDataStore.LINUX_APPLICATIONS_DIRECTORY
+import com.example.core.data.auth.DesktopDataStore.MAC_APPLICATIONS_DIRECTORY
+import com.example.core.data.util.DesktopOS
+import com.example.core.data.util.currentOS
+import java.io.File
+
+fun createDataStore(): DataStore<Preferences> = createDataStore {
+    val userHomeDirectory = System.getProperty(HOME_DIRECTORY)
+    val applicationDirectory = when (currentOS) {
+        DesktopOS.WINDOWS -> File(System.getenv(APP_DATA), APP_FOLDER_NAME)
+        DesktopOS.MACOS -> File(
+            userHomeDirectory,
+            buildString { MAC_APPLICATIONS_DIRECTORY + APP_FOLDER_NAME })
+
+        DesktopOS.LINUX -> File(
+            userHomeDirectory,
+            buildString { LINUX_APPLICATIONS_DIRECTORY + APP_FOLDER_NAME })
+    }
+
+    if (!applicationDirectory.exists()) {
+        applicationDirectory.mkdirs()
+    }
+
+    File(applicationDirectory, DATA_STORE_FILE_NAME).absolutePath
+}
+
+object DesktopDataStore {
+    const val APP_FOLDER_NAME = "Chirp"
+    const val HOME_DIRECTORY = "user.home"
+    const val APP_DATA = "APPDATA"
+    const val MAC_APPLICATIONS_DIRECTORY = "Library/Application Support/"
+    const val LINUX_APPLICATIONS_DIRECTORY = ".local/share/"
+}
