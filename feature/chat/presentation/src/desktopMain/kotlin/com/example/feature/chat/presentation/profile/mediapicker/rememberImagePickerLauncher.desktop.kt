697726bb73bb0f765d7f3d351d0c22a9a71a1c56
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-07 23:48:07 +0200

    feature(mediapicker): create function that converts Java's File into PickedImageData

--- feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
+++ feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
@@ -1,6 +1,8 @@
 package com.example.feature.chat.presentation.profile.mediapicker
 
 import androidx.compose.runtime.remember
+import java.io.File
+import java.nio.file.Files
 
 @androidx.compose.runtime.Composable
 actual fun rememberImagePicker(onResult: (PickedImageData) -> Unit): ImagePickerLauncher {
@@ -15,14 +17,24 @@ enum class ImageExtension {
     PNG,
     JPEG,
     JPG,
-    WEBP;
+    WEBP
+}
 
-    fun getMimeTypeFromFileName(filename: String): String? {
-        val extension = filename.substringAfterLast(".", "").lowercase()
-        val imageExtension = ImageExtension.entries.firstOrNull { it.name.lowercase() == extension }
+fun getMimeTypeFromFileName(filename: String): String? {
+    val extension = filename.substringAfterLast(".", "").lowercase()
+    val imageExtension = ImageExtension.entries.firstOrNull { it.name.lowercase() == extension }
 
-        return imageExtension?.let { extension ->
-            "image/$extension"
-        }
+    return imageExtension?.let { extension ->
+        "image/$extension"
     }
+}
+
+fun File.toPickedImageData(): PickedImageData {
+    val mimeType = getMimeTypeFromFileName(filename = name)
+    val bytes = Files.readAllBytes(toPath())
+
+    return PickedImageData(
+        bytes = bytes,
+        mimeType = mimeType
+    )
 }
