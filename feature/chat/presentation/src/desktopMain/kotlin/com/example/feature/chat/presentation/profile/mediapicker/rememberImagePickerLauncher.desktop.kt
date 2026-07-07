4497dc7fcf6f391791fe8dd40bbc8ae1a521dae1
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-07 23:51:50 +0200

    feature(mediapicker): create private logic that displays dialog and let user pick an image

--- feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
+++ feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
@@ -4,9 +4,15 @@ import androidx.compose.runtime.remember
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
+import java.awt.FileDialog
+import java.awt.Frame
 import java.io.File
+import java.io.FilenameFilter
 import java.nio.file.Files
+import javax.swing.SwingUtilities
+import kotlin.coroutines.resume
 
 @androidx.compose.runtime.Composable
 actual fun rememberImagePicker(onResult: (PickedImageData) -> Unit): ImagePickerLauncher {
@@ -17,13 +23,43 @@ actual fun rememberImagePicker(onResult: (PickedImageData) -> Unit): ImagePicker
     }
 }
 
-enum class ImageExtension {
+private enum class ImageExtension {
     PNG,
     JPEG,
     JPG,
     WEBP
 }
 
+private suspend fun pickImage(fileDialogTitle: String): PickedImageData? {
+    val file = suspendCancellableCoroutine { continuation ->
+        var fileDialog: FileDialog? = null
+
+        continuation.invokeOnCancellation {
+            // emit global event
+            SwingUtilities.invokeLater {
+                fileDialog?.dispose()
+            }
+        }
+
+        SwingUtilities.invokeLater {
+            try {
+                fileDialog = FileDialog(Frame(), fileDialogTitle, FileDialog.LOAD)
+                fileDialog.apply {
+                    filenameFilter = FilenameFilter { _, name ->
+                        ImageExtension.entries.any { extension -> name.endsWith(extension.name.lowercase()) }
+                    }
+                    isVisible = true
+                }
+                val file = File(fileDialog.directory, fileDialog.file)
+                continuation.resume(file)
+            } catch (_: Exception) {
+                continuation.resume(null)
+            }
+        }
+    }
+
+    return file?.suspendToPickedImage()
+}
 
 private suspend fun File.suspendToPickedImage() = withContext(Dispatchers.IO) {
     return@withContext try {
