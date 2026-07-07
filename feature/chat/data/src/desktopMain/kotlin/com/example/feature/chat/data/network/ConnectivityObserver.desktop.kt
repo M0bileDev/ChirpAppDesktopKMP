3b19198d96dfc52039e955bb947a58ea0bd81608
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-07 22:40:05 +0200

    feature(network): create constants of dns values used to test network connection

--- feature/chat/data/src/desktopMain/kotlin/com/example/feature/chat/data/network/ConnectivityObserver.desktop.kt
+++ feature/chat/data/src/desktopMain/kotlin/com/example/feature/chat/data/network/ConnectivityObserver.desktop.kt
@@ -6,4 +6,11 @@ import kotlinx.coroutines.flow.flowOf
 actual class ConnectivityObserver {
     actual val isConnected: Flow<Boolean>
         get() = flowOf(true)
+
+    companion object {
+        const val GOOGLE_DNS = "8.8.8.8"
+        const val CLAUD_FLARE_DNS = "1.1.1.1"
+        const val OPEN_DNS_DNS = "208.67.222.222"
+        const val STANDARD_DNS_PORT = 53
+    }
 }
