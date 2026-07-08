67002b6f58146b94d545c5bee3709078e0e4c870
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-08 21:07:37 +0200

    refactor(mediapicker): update imports

--- feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
+++ feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
@@ -4,18 +4,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import chirpappkmp.feature.chat.presentation.generated.resources.Res
 import chirpappkmp.feature.chat.presentation.generated.resources.select_a_profile_image
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.ensureActive
+import com.example.feature.chat.presentation.profile.mediapicker.ext.hasValidImageExtension
+import com.example.feature.chat.presentation.profile.mediapicker.ext.suspendToPickedImage
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlinx.coroutines.withContext
 import org.jetbrains.compose.resources.stringResource
 import java.awt.FileDialog
 import java.awt.Frame
 import java.io.File
 import java.io.FilenameFilter
-import java.nio.file.Files
 import javax.swing.SwingUtilities
 import kotlin.coroutines.resume
 
@@ -37,12 +34,6 @@ actual fun rememberImagePicker(onResult: (PickedImageData) -> Unit): ImagePicker
     }
 }
 
-private enum class ImageExtension {
-    PNG,
-    JPEG,
-    JPG,
-    WEBP
-}
 
 private suspend fun pickImage(fileDialogTitle: String): PickedImageData? {
     val file = suspendCancellableCoroutine { continuation ->
@@ -59,9 +50,7 @@ private suspend fun pickImage(fileDialogTitle: String): PickedImageData? {
             try {
                 fileDialog = FileDialog(Frame(), fileDialogTitle, FileDialog.LOAD)
                 fileDialog.apply {
-                    filenameFilter = FilenameFilter { _, name ->
-                        ImageExtension.entries.any { extension -> name.endsWith(extension.name.lowercase()) }
-                    }
+                    filenameFilter = FilenameFilter { _, name -> name.hasValidImageExtension() }
                     isVisible = true
                 }
                 val file = File(fileDialog.directory, fileDialog.file)
@@ -73,32 +62,4 @@ private suspend fun pickImage(fileDialogTitle: String): PickedImageData? {
     }
 
     return file?.suspendToPickedImage()
-}
-
-private suspend fun File.suspendToPickedImage() = withContext(Dispatchers.IO) {
-    return@withContext try {
-        toPickedImageData()
-    } catch (_: Exception) {
-        currentCoroutineContext().ensureActive()
-        null
-    }
-}
-
-private fun File.toPickedImageData(): PickedImageData {
-    val mimeType = getMimeTypeFromFileName(filename = name)
-    val bytes = Files.readAllBytes(toPath())
-
-    return PickedImageData(
-        bytes = bytes,
-        mimeType = mimeType
-    )
-}
-
-private fun getMimeTypeFromFileName(filename: String): String? {
-    val extension = filename.substringAfterLast(".", "").lowercase()
-    val imageExtension = ImageExtension.entries.firstOrNull { it.name.lowercase() == extension }
-
-    return imageExtension?.let { extension ->
-        "image/${extension.name.lowercase()}"
-    }
 }
