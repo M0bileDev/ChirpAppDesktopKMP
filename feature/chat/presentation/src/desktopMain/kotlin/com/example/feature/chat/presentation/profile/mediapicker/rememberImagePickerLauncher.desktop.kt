bb215ab2ee4f4010d8f26f21cd33b6fa8a45a162
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-08 20:25:52 +0200

    feature(mediapicker): provide implementation of rememberImagePicker for desktop

--- feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
+++ feature/chat/presentation/src/desktopMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.desktop.kt
@@ -1,11 +1,16 @@
 package com.example.feature.chat.presentation.profile.mediapicker
 
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.select_a_profile_image
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
+import org.jetbrains.compose.resources.stringResource
 import java.awt.FileDialog
 import java.awt.Frame
 import java.io.File
@@ -16,9 +21,18 @@ import kotlin.coroutines.resume
 
 @androidx.compose.runtime.Composable
 actual fun rememberImagePicker(onResult: (PickedImageData) -> Unit): ImagePickerLauncher {
+    val scope = rememberCoroutineScope()
+    val dialogTitle = stringResource(Res.string.select_a_profile_image)
+
     return remember {
         ImagePickerLauncher(
-            onLaunch = {}
+            onLaunch = {
+                scope.launch {
+                    pickImage(fileDialogTitle = dialogTitle)?.let { data ->
+                        onResult(data)
+                    }
+                }
+            }
         )
     }
 }
