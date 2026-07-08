89c345d8d1b6bf0fab55d15b2bb70253ebb98c61
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-08 20:47:04 +0200

    feature(mediapicker): create definition of platform specific composable rememberDragAndDropTarget function

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberDragAndDropTarget.kt
@@ -0,0 +1,10 @@
+package com.example.feature.chat.presentation.profile.mediapicker
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.draganddrop.DragAndDropTarget
+
+@Composable
+expect fun rememberDragAndDropTarget(
+    onHover: (Boolean) -> Unit,
+    onDrop: (ByteArray) -> Unit
+): DragAndDropTarget
