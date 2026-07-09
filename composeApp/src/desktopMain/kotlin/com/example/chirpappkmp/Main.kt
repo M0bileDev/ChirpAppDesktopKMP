64c7eda99dec896fad4c9df978e35fc8871153ff
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-09 22:43:16 +0200

    feature(chirpappkmp): register module in Koin Application

--- composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
@@ -2,15 +2,20 @@ package com.example.chirpappkmp
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.example.chirpappkmp.di.desktopModule
 import com.example.chirpappkmp.di.initKoin
 
-fun main(){
-    initKoin()
+fun main() {
+    initKoin {
+        modules(
+            desktopModule
+        )
+    }
     application {
         Window(
             onCloseRequest = ::exitApplication,
             title = "ChirpDesktop"
-        ){
+        ) {
             App()
         }
     }
