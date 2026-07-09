151feedfabb88a9c46689bdaecd99a1b84dccd91
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-09 22:58:44 +0200

    feature(chirpkmp): extract Window to separate composable ChirpWindow

--- composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
@@ -1,9 +1,9 @@
 package com.example.chirpappkmp
 
-import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.example.chirpappkmp.di.desktopModule
 import com.example.chirpappkmp.di.initKoin
+import com.example.chirpappkmp.windows.ChirpWindow
 
 fun main() {
     initKoin {
@@ -12,11 +12,10 @@ fun main() {
         )
     }
     application {
-        Window(
+        ChirpWindow(
             onCloseRequest = ::exitApplication,
-            title = "ChirpDesktop"
-        ) {
-            App()
-        }
+            onAddWindowClick = {},
+            onFocusChanged = {}
+        )
     }
 }
