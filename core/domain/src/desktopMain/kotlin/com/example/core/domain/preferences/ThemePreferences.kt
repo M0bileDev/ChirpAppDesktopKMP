3ffeb34f671329a374514ca35e1a84a28db442e3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-10 23:39:32 +0200

    feature(preferences): create interface that represents data store of desktop theme

---
+++ core/domain/src/desktopMain/kotlin/com/example/core/domain/preferences/ThemePreferences.kt
@@ -0,0 +1,8 @@
+package com.example.core.domain.preferences
+
+import kotlinx.coroutines.flow.Flow
+
+interface ThemePreferences {
+    fun observeThemePreference(): Flow<ThemePreference>
+    suspend fun updateThemePreference(theme: ThemePreference)
+}
