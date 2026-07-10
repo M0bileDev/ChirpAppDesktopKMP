488501ab116e6087e7b2a0b52e1c77f99a81efde
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-10 23:20:10 +0200

    feature(chirpappkmp): inject property of ApplicationStateHolder (without lifecycle, there isn't any for desktop); create application window based on application state

--- composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/Main.kt
@@ -1,9 +1,12 @@
 package com.example.chirpappkmp
 
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.window.application
 import com.example.chirpappkmp.di.desktopModule
 import com.example.chirpappkmp.di.initKoin
 import com.example.chirpappkmp.windows.ChirpWindow
+import org.koin.compose.koinInject
 
 fun main() {
     initKoin {
@@ -12,10 +15,18 @@ fun main() {
         )
     }
     application {
-        ChirpWindow(
-            onCloseRequest = ::exitApplication,
-            onAddWindowClick = {},
-            onFocusChanged = {}
-        )
+        val applicationStateHolder = koinInject<ApplicationStateHolder>()
+        val applicationState by applicationStateHolder.state.collectAsState()
+        val windows = applicationState.windows
+
+        for (window in windows) {
+            ChirpWindow(
+                onCloseRequest = {
+                    applicationStateHolder.onRemoveWindowClick(window.id)
+                },
+                onAddWindowClick = applicationStateHolder::onAddWindowClick,
+                onFocusChanged = {}
+            )
+        }
     }
 }
