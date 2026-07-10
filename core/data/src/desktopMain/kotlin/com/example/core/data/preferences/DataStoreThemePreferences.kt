430e36a40e46265b4cf64db3fda4cb3f9c821e2f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-10 23:44:12 +0200

    feature(preferences): provide implementation of observeThemePreference function (return flow of theme)

--- core/data/src/desktopMain/kotlin/com/example/core/data/preferences/DataStoreThemePreferences.kt
+++ core/data/src/desktopMain/kotlin/com/example/core/data/preferences/DataStoreThemePreferences.kt
@@ -2,16 +2,31 @@ package com.example.core.data.preferences
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.core.domain.preferences.ThemePreference
 import com.example.core.domain.preferences.ThemePreferences
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class DataStoreThemePreferences(
     private val dataStore: DataStore<Preferences>
 ) : ThemePreferences {
 
+    private val themePreferenceKey = stringPreferencesKey("theme_preference")
+
     override fun observeThemePreference(): Flow<ThemePreference> {
-        TODO("Not yet implemented")
+        return dataStore
+            .data
+            .map { preferences ->
+                val currentPreference =
+                    preferences[themePreferenceKey] ?: ThemePreference.SYSTEM.name
+
+                try {
+                    ThemePreference.valueOf(currentPreference)
+                } catch (_: Exception) {
+                    ThemePreference.SYSTEM
+                }
+            }
     }
 
     override suspend fun updateThemePreference(theme: ThemePreference) {
