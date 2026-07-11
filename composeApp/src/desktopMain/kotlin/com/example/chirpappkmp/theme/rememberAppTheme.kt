ccd1b80684d1ff839d0161ce158b0eff8837e0e9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-11 22:50:29 +0200

    feature(theme): create function that represents composable remember version of AppTheme

---
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/theme/rememberAppTheme.kt
@@ -0,0 +1,47 @@
+package com.example.chirpappkmp.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.core.domain.preferences.ThemePreference
+import com.jthemedetecor.OsThemeDetector
+import java.util.function.Consumer
+
+@Composable
+fun rememberAppTheme(
+    themePreferenceFromAppSettings: ThemePreference
+): AppTheme {
+    var isSystemThemeDark by remember {
+        if (OsThemeDetector.isSupported()) {
+            mutableStateOf(OsThemeDetector.getDetector().isDark)
+        } else {
+            val isSettingsPreferenceDark = themePreferenceFromAppSettings == ThemePreference.DARK
+            mutableStateOf(isSettingsPreferenceDark)
+        }
+    }
+
+    DisposableEffect(Unit) {
+        var listener: Consumer<Boolean>? = null
+        if (OsThemeDetector.isSupported()) {
+            listener = Consumer<Boolean> { dark -> isSystemThemeDark = dark }
+            OsThemeDetector.getDetector().registerListener(listener)
+        }
+
+        onDispose {
+            OsThemeDetector.getDetector().removeListener(listener)
+        }
+    }
+
+    val isDarkTheme = when (themePreferenceFromAppSettings) {
+        ThemePreference.LIGHT -> false
+        ThemePreference.DARK -> true
+        ThemePreference.SYSTEM -> isSystemThemeDark
+    }
+
+    return remember {
+        if (isDarkTheme) AppTheme.DARK else AppTheme.LIGHT
+    }
+}
