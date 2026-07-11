302b9a6fb3c1210a34d7779d29bf40a63f9618ce
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-11 22:28:13 +0200

    refactor(chirpappkmp): add property that represents desktop theme

--- composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/ApplicationState.kt
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/ApplicationState.kt
@@ -1,7 +1,9 @@
 package com.example.chirpappkmp
 
 import com.example.chirpappkmp.windows.WindowState
+import com.example.core.domain.preferences.ThemePreference
 
 data class ApplicationState(
-    val windows: List<WindowState> = listOf(WindowState())
+    val windows: List<WindowState> = listOf(WindowState()),
+    val themePreferences: ThemePreference = ThemePreference.SYSTEM
 )
