a78d13710b5c028a41949a9767e4312d40becad8
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-11 22:31:16 +0200

    feature(chirpappkmp): add logic that observes theme preferences from data store

--- composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/ApplicationStateHolder.kt
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/ApplicationStateHolder.kt
@@ -1,24 +1,45 @@
 package com.example.chirpappkmp
 
 import com.example.chirpappkmp.windows.WindowState
+import com.example.core.domain.preferences.ThemePreferences
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
 class ApplicationStateHolder(
-    private val applicationScope: CoroutineScope
+    private val applicationScope: CoroutineScope,
+    private val themePreferences: ThemePreferences
 ) {
     private val _state = MutableStateFlow(ApplicationState())
     val state = _state
-        .onStart { }
+        .onStart {
+            observeThemePreference()
+        }
         .stateIn(
             applicationScope,
             SharingStarted.Lazily,
             ApplicationState()
         )
+
+    fun observeThemePreference() {
+        themePreferences
+            .observeThemePreference()
+            .onEach { themePreference ->
+                _state.update {
+                    it.copy(
+                        themePreferences = themePreference
+                    )
+                }
+            }
+            .launchIn(
+                applicationScope
+            )
+    }
 
     fun onAddWindowClick() {
         _state.update {
