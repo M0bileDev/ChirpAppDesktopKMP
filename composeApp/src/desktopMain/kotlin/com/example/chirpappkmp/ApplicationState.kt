3d6b681c114016f638c6ce5fc206e0124a91878c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-11 23:44:40 +0200

    feature(chirpappkmp): add property that represents TrayState

--- composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/ApplicationState.kt
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/ApplicationState.kt
@@ -1,9 +1,11 @@
 package com.example.chirpappkmp
 
+import androidx.compose.ui.window.TrayState
 import com.example.chirpappkmp.windows.WindowState
 import com.example.core.domain.preferences.ThemePreference
 
 data class ApplicationState(
     val windows: List<WindowState> = listOf(WindowState()),
-    val themePreferences: ThemePreference = ThemePreference.SYSTEM
+    val themePreferences: ThemePreference = ThemePreference.SYSTEM,
+    val trayState: TrayState = TrayState()
 )
