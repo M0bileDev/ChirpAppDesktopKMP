5269d33740ee605163b69619a64660b4e40f24ca
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-13 19:49:13 +0200

    feature(deeplink): create desktop version of uri handler
    Created setup function that checks desktop requirements

---
+++ composeApp/src/desktopMain/kotlin/com/example/chirpappkmp/deeplink/DesktopDeeplinkHandler.kt
@@ -0,0 +1,33 @@
+package com.example.chirpappkmp.deeplink
+
+import java.awt.Desktop
+import javax.swing.SwingUtilities
+
+object DesktopDeeplinkHandler {
+    private var isInitialize = false
+
+    fun setup() {
+        if (!Desktop.isDesktopSupported()) return
+        if (isInitialize) return
+
+        try {
+            val desktop = Desktop.getDesktop()
+            if (!desktop.isSupported(Desktop.Action.APP_OPEN_URI)) return
+
+            desktop.setOpenURIHandler { event ->
+                val uri = event.uri.toString()
+                SwingUtilities.invokeLater {
+                    processUri(uri)
+                }
+            }
+        } catch (
+            e: Exception
+        ) {
+            e.printStackTrace()
+        }
+    }
+
+    fun processUri(uri: String) {
+        TODO("Not yet implemented")
+    }
+}
