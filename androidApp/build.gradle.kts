e6649f2b7ed209ba175e5d4d3363fb53d1a4e448
m0bileDev <damian.ogorek.1996@gmail.com>
2026-07-17 23:05:15 +0200

    feature(androidApp): add dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.convention.android.application)
+    alias(libs.plugins.google.services)
     alias(libs.plugins.compose.compiler)
 }
 
@@ -22,10 +23,24 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
+    implementation(projects.composeApp)
+
+    implementation(platform(libs.androidx.compose.bom))
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.tooling.preview)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+
+    implementation(libs.koin.android)
+
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.core.splashscreen)
     implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
